f5cf7a8ba2a8ac7b0e00c5fca13120b44d518600
Nicholas Benson <nbenson@leapmotion.com>
2018-05-04 15:04:50 -0700

    Re-added missing assets and removed particles-only stuff

---
+++ Assets/Galaxy/UI/Common Assets/Scripts/FaceCameraOnUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCameraOnUpdate : MonoBehaviour {
+
+  private Camera _mainCamera;
+
+  public bool faceAway = false;
+
+  void Start() {
+    _mainCamera = Camera.main;
+  }
+
+  void Update() {
+    if (_mainCamera == null) {
+      _mainCamera = Camera.main;
+    }
+    this.transform.rotation = Quaternion.LookRotation((_mainCamera.transform.position
+                                                         - this.transform.position)
+                                                       * (faceAway ? -1f : 1f),
+                                                      Vector3.up);
+  }
+
+}
